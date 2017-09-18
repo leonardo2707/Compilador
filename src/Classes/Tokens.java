3b96db882dfa557bdf992e72b8067aa8e51f6fc0
Emerson Leonardo Zock Alves <emerson@ossan>
2017-09-18 20:19:38 -0300

    criado os get's e set's de um token

--- src/Classes/Tokens.java
+++ src/Classes/Tokens.java
@@ -2,6 +2,25 @@ package Classes;
 
 public class Tokens {
     
+    private int codToken;
+    private String token;
+
+    public int getCodToken() {
+        return codToken;
+    }
+
+    public void setCodToken(int codToken) {
+        this.codToken = codToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
     
     
 }
