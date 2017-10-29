e0cf800228dbb2e3de0786679ddd4cc0f59a9f9c
leonardo2707 <leonardo_2707@hotmail.com>
2017-10-29 16:02:00 -0200

    enviando fiz para programar no desktop

--- src/Classes/GramaticaCodificada.java
+++ src/Classes/GramaticaCodificada.java
@@ -15,86 +15,6 @@ public class GramaticaCodificada {
     
     ArrayList<Regra> regras = new ArrayList<Regra>();
     
-    ArrayList<Token> regra1 = new ArrayList<Token>();
-    ArrayList<Token> regra2 = new ArrayList<Token>();
-    ArrayList<Token> regra3 = new ArrayList<Token>();
-    ArrayList<Token> regra4 = new ArrayList<Token>();
-    ArrayList<Token> regra5 = new ArrayList<Token>();
-    ArrayList<Token> regra6 = new ArrayList<Token>();
-    ArrayList<Token> regra7 = new ArrayList<Token>();
-    ArrayList<Token> regra8 = new ArrayList<Token>();
-    ArrayList<Token> regra9 = new ArrayList<Token>();
-    ArrayList<Token> regra10 = new ArrayList<Token>();
-    ArrayList<Token> regra11= new ArrayList<Token>();
-    ArrayList<Token> regra12 = new ArrayList<Token>();
-    ArrayList<Token> regra13 = new ArrayList<Token>();
-    ArrayList<Token> regra14 = new ArrayList<Token>();
-    ArrayList<Token> regra15 = new ArrayList<Token>();
-    ArrayList<Token> regra16 = new ArrayList<Token>();
-    ArrayList<Token> regra17 = new ArrayList<Token>();
-    ArrayList<Token> regra18 = new ArrayList<Token>();
-    ArrayList<Token> regra19 = new ArrayList<Token>();
-    ArrayList<Token> regra20 = new ArrayList<Token>();
-    ArrayList<Token> regra21 = new ArrayList<Token>();
-    ArrayList<Token> regra22 = new ArrayList<Token>();
-    ArrayList<Token> regra23 = new ArrayList<Token>();
-    ArrayList<Token> regra24 = new ArrayList<Token>();
-    ArrayList<Token> regra25 = new ArrayList<Token>();
-    ArrayList<Token> regra26 = new ArrayList<Token>();
-    ArrayList<Token> regra27 = new ArrayList<Token>();
-    ArrayList<Token> regra28 = new ArrayList<Token>();
-    ArrayList<Token> regra29 = new ArrayList<Token>();
-    ArrayList<Token> regra30 = new ArrayList<Token>();
-    ArrayList<Token> regra31 = new ArrayList<Token>();
-    ArrayList<Token> regra32 = new ArrayList<Token>();
-    ArrayList<Token> regra33 = new ArrayList<Token>();
-    ArrayList<Token> regra34 = new ArrayList<Token>();
-    ArrayList<Token> regra35 = new ArrayList<Token>();
-    ArrayList<Token> regra36 = new ArrayList<Token>();
-    ArrayList<Token> regra37 = new ArrayList<Token>();
-    ArrayList<Token> regra38 = new ArrayList<Token>();
-    ArrayList<Token> regra39 = new ArrayList<Token>();
-    ArrayList<Token> regra40 = new ArrayList<Token>();
-    ArrayList<Token> regra41 = new ArrayList<Token>();
-    ArrayList<Token> regra42 = new ArrayList<Token>();
-    ArrayList<Token> regra43 = new ArrayList<Token>();
-    ArrayList<Token> regra44 = new ArrayList<Token>();
-    ArrayList<Token> regra45 = new ArrayList<Token>();
-    ArrayList<Token> regra46 = new ArrayList<Token>();
-    ArrayList<Token> regra47 = new ArrayList<Token>();
-    ArrayList<Token> regra48 = new ArrayList<Token>();
-    ArrayList<Token> regra49 = new ArrayList<Token>();
-    ArrayList<Token> regra50 = new ArrayList<Token>();
-    ArrayList<Token> regra51 = new ArrayList<Token>();
-    ArrayList<Token> regra52 = new ArrayList<Token>();
-    ArrayList<Token> regra53 = new ArrayList<Token>();
-    ArrayList<Token> regra54 = new ArrayList<Token>();
-    ArrayList<Token> regra55 = new ArrayList<Token>();
-    ArrayList<Token> regra56 = new ArrayList<Token>();
-    ArrayList<Token> regra57 = new ArrayList<Token>();
-    ArrayList<Token> regra58 = new ArrayList<Token>();
-    ArrayList<Token> regra59 = new ArrayList<Token>();
-    ArrayList<Token> regra60 = new ArrayList<Token>();
-    ArrayList<Token> regra61 = new ArrayList<Token>();
-    ArrayList<Token> regra62 = new ArrayList<Token>();
-    ArrayList<Token> regra63 = new ArrayList<Token>();
-    ArrayList<Token> regra64 = new ArrayList<Token>();
-    ArrayList<Token> regra65 = new ArrayList<Token>();
-    ArrayList<Token> regra66 = new ArrayList<Token>();
-    ArrayList<Token> regra67 = new ArrayList<Token>();
-    ArrayList<Token> regra68 = new ArrayList<Token>();
-    ArrayList<Token> regra69 = new ArrayList<Token>();
-    ArrayList<Token> regra70 = new ArrayList<Token>();
-    ArrayList<Token> regra71 = new ArrayList<Token>();
-    ArrayList<Token> regra72 = new ArrayList<Token>();
-    ArrayList<Token> regra73 = new ArrayList<Token>();
-    ArrayList<Token> regra74 = new ArrayList<Token>();
-    ArrayList<Token> regra75 = new ArrayList<Token>();
-    ArrayList<Token> regra76 = new ArrayList<Token>();
-    ArrayList<Token> regra77 = new ArrayList<Token>();
-    ArrayList<Token> regra78 = new ArrayList<Token>();
-    ArrayList<Token> regra79 = new ArrayList<Token>();
-    ArrayList<Token> regra80 = new ArrayList<Token>();
     
     Dicionario dicionario = new Dicionario();
     
@@ -272,12 +192,67 @@ public class GramaticaCodificada {
         regras.add(38, new Regra(dicionario.retornaTokenDicionarioCodigo(15)));
         //regra38.add(dicionario.retornaTokenDicionarioCodigo(15));
         
-        regra39.add(dicionario.retornaTokenDicionarioCodigo(64));
+        regras.add(39, new Regra(dicionario.retornaTokenDicionarioCodigo(64), dicionario.retornaTokenDicionarioCodigo(7),
+        dicionario.retornaTokenDicionarioCodigo(24)));
+        /*regra39.add(dicionario.retornaTokenDicionarioCodigo(64));
         regra39.add(dicionario.retornaTokenDicionarioCodigo(7));
-        regra39.add(dicionario.retornaTokenDicionarioCodigo(24));
+        regra39.add(dicionario.retornaTokenDicionarioCodigo(24));*/
         
-        regra40.add(dicionario.retornaTokenDicionarioCodigo(15));
+        regras.add(40, new Regra(dicionario.retornaTokenDicionarioCodigo(15)));
+        //regra40.add(dicionario.retornaTokenDicionarioCodigo(15));
         
+        /*
+        41 = 43,65,66,42
+        42 = 15
+        43 = 40,65,66
+        44 = 5
+        45 = 9
+        46 = 6
+        47 = 8
+        47 = 7
+        49 = 14,43,7,67,36,61,37,62,35,68
+        50 = 19, 36, 61, 37, 62, 35
+        51 = 15
+        52 = 1,43,7,67,42,36,61,37,62,35
+        53 = 28,69
+        54 = 45,69
+        55 = 27,69
+        56 = 26,69
+        57 = 32,69
+        58 = 30,69
+        59 = 5
+        60 = 6
+        61 = 9
+        62 = 8
+        63 = 7
+        64 = 16,43,7,29,69,37,7,67,37,70,42,36,61,37,62,35
+        65 = 33,5
+        66 = 46,5
+        67 = 20,36,61,37,62,35,1,43,7,67,42
+        68 = 22,25,7
+        69 = 21,31,11,71
+        70 = 15
+        71 = 31,7,72,71
+        72 = 31,11,71
+        73 = 15
+        74 = 40,7,72
+        75 = 73,74
+        76 = 24,7,64
+        77 = 34, 73, 74
+        78 = 47,73,74
+        79 = 15
+        80 = 75, 76
+        81 = 15
+        82 = 41,75,76
+        83 = 39,75,76
+        84 = 5
+        85 = 6
+        86 = 7
+        87 = 9
+        88 = 8
+        89 = 43,63,42
+        
+        */
         // Continuar do 41 até o 80...
     }
     
